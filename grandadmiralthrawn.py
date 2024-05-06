75abef97a56ed29ceb4d65f0e4cdef5f8be2188b
GregoryK36 <168002368+GregoryK36@users.noreply.github.com>
2024-05-06 09:23:42 -0400

    Add files via upload

---
+++ grandadmiralthrawn.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+#refactor the image
+
+class Thrawn:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("GrandAdmiralThrawn.png")
+        self.rescale_image(self.image)
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.delta = 3
+
+    def rescale_image(self, image):
+        self.image_size = self.image.get_size()
+        scale_size = (self.image_size[0] * .3, self.image_size[1] * .3)
+        self.image = pygame.transform.scale(self.image, scale_size)
+
+    def move_balloon(self, direction):
+        if direction == "right":
+            self.x = self.x + self.delta
+        if direction == "left":
+            self.x = self.x - self.delta
+        if direction == "up":
+            self.y = self.y - self.delta
+        if direction == "down":
+            self.y = self.y + self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Star Wars is cool")
+import pygame
+import random
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 20)
+pygame.display.set_caption("Balloon Flight!")
+# set up variables for the display
+size = (1000, 1000)
+screen = pygame.display.set_mode(size)
+from grandadmiralthrawn import Thrawn
+b = Thrawn(500, 500)
+run = True
+while run:
+     keys = pygame.key.get_pressed()  # checking pressed keys
+     if keys[pygame.K_SPACE]:
+        b.move_balloon("up")
+     else:
+         b.move_balloon("down")
+     for event in pygame.event.get():  # User did something
+         if event.type == pygame.QUIT:  # If user clicked close
+             run = False
+     screen.blit(b.image, b.rect)
+     pygame.display.update()
+
+
+
+
+
+
+
+
