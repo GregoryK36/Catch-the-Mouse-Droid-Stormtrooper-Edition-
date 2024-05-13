ca72a33a95eb8606c6df0ce3e56a7f216af51185
GregoryK36 <168002368+GregoryK36@users.noreply.github.com>
2024-05-13 09:23:44 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ pygame.font.init()
 my_font = pygame.font.SysFont('Arial', 20)
 pygame.display.set_caption("Balloon Flight!")
 # set up variables for the display
-size = (1000, 1000)
+size = (700, 700)
 screen = pygame.display.set_mode(size)
 from grandadmiralthrawn import Thrawn
+from chopper import Chopper
 b = Thrawn(500, 500)
+c = Chopper(500, 500)
 run = True
 while run:
-     keys = pygame.key.get_pressed()  # checking pressed keys
-     if keys[pygame.K_SPACE]:
-        b.move_balloon("up")
-     else:
-         b.move_balloon("down")
-     for event in pygame.event.get():  # User did something
+  keys = pygame.key.get_pressed()  # checking pressed keys
+  if keys[pygame.K_d]:
+      b.move_balloon("right")
+      c.move_balloon("right")
+  if keys[pygame.K_a]:
+      b.move_balloon("left")
+      c.move_balloon("left")
+  for event in pygame.event.get():  # User did something
          if event.type == pygame.QUIT:  # If user clicked close
              run = False
-     screen.blit(b.image, b.rect)
-     pygame.display.update()
+
+         else:
+             screen.blit(b.image, b.rect)
+             screen.blit(c.image, c.rect)
+         pygame.display.update()
 
 
 
